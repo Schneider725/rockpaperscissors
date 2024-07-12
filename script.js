c564c13da5027c15885f5f478cf153c0db93d0e7
stephen etienne <sschneider725@gmail.com>
2024-07-12 18:56:37 -0400

    improve readability

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let humanScore = 0
 let computerScore = 0
-function getComputerChoice(){
+
+function getComputerChoice() {
     let compNum = Math.floor(Math.random() * 3)
-    if(compNum === 0) {
+    if (compNum === 0) {
         alert("ROCK!")
     }
-    else if(compNum === 1) {
+    else if (compNum === 1) {
         alert("PAPER")
     }
     else {
@@ -13,16 +14,17 @@ function getComputerChoice(){
     }
     return compNum
 }
-function getHumanChoice(){
+function getHumanChoice() {
     let ans = prompt("rock paper scissors SHOOT!")
+    ans = ans.toLocaleLowerCase()
     let humNum
-    if(ans === "rock"){
+    if (ans === "rock") {
         humNum = 0
     }
-    else if(ans ==="paper"){
+    else if (ans === "paper") {
         humNum = 1
     }
-    else if(ans ==="scissors"){
+    else if (ans === "scissors") {
         humNum = 2
     }
     else {
@@ -33,46 +35,50 @@ function getHumanChoice(){
     return humNum
 }
 
-function playRound(humanChoice,computerChoice){
-    if(humanChoice == 0 && computerChoice == 0){
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
         alert("tie")
     }
-    else if(humanChoice == 1 && computerChoice == 0){
+    else if (humanChoice == 1 && computerChoice == 0) {
         alert("you win. paper beats rock")
         humanScore++
     }
-    else if(humanChoice == 2 && computerChoice == 0){
+    else if (humanChoice == 2 && computerChoice == 0) {
         alert("you lose")
         computerScore++
     }
-    else if(humanChoice == 0 && computerChoice == 1){
+    else if (humanChoice == 0 && computerChoice == 1) {
         alert("you lose")
         computerScore++
     }
-    else if(humanChoice == 1 && computerChoice == 1){
-        alert("tie")
-    }
-    else if(humanChoice == 2 && computerChoice == 1){
+    else if (humanChoice == 2 && computerChoice == 1) {
         alert("you win. scissors beats paper")
         humanScore++
     }
-    else if(humanChoice == 0 && computerChoice == 2){
+    else if (humanChoice == 0 && computerChoice == 2) {
         alert("you win. rock beats scissors")
         humanScore++
     }
-    else if(humanChoice == 1 && computerChoice == 2){
+    else if (humanChoice == 1 && computerChoice == 2) {
         alert("you lose")
-        computerChoice++
+        computerScore++
     }
-    else if(humanChoice == 2 && computerChoice == 2){
-        alert("tie")
-    }
+
     alert(`Human : ${humanScore} Computer : ${computerScore}`)
 
 }
 
-
-const humanSelection = getHumanChoice()
-const computerSelction = getComputerChoice()
-
-playRound(humanSelection,computerSelction)
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let humanSelection = getHumanChoice()
+        let computerSelection = getComputerChoice()
+        playRound(humanSelection, computerSelection)
+    }
+    if(humanScore > computerScore){
+        alert("HUMANS WIN")
+    }
+    else {
+        alert("ROBOTS WIN")
+    }
+}
+game()
